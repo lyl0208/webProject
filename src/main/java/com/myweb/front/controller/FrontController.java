c80021ba4c3ad18dd92959cd3ad32af1dde3c1fc
linyilun <linyilun@iqurong.com>
2018-05-09 14:57:16 +0800

    done

---
+++ src/main/java/com/myweb/front/controller/FrontController.java
@@ -0,0 +1,31 @@
+package com.myweb.front.controller;
+
+import com.myweb.phone.model.PhoneBrand;
+import com.myweb.phone.model.PhoneModel;
+import com.myweb.phone.service.PhoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/front")
+public class FrontController {
+
+    @Autowired
+    private PhoneService phoneService;
+
+    @PostMapping("/listBrand")
+    public List<PhoneBrand> listBrand() {
+        return phoneService.getBrands();
+    }
+
+    @PostMapping("/listPhoneItem")
+    public List<PhoneModel> listPhoneItem(Long brandId) {
+        return phoneService.getModels(brandId);
+    }
+
+
+}
---
+++ web/static/js/index.js
@@ -0,0 +1,40 @@
+$(function () {
+
+    var brandId = null;
+
+    $.post({
+        url:"/front/listBrand"
+        ,async:false
+        ,success:function (data) {
+        layui.laytpl(brandList.innerHTML).render(data,function (html) {
+            $('#brand-list').html(html);
+            brandId = data[0].brandId;
+        })}
+    });
+
+    layui.use('element',function () {
+        var element = layui.element;
+
+        element.init();
+
+    });
+
+    listPhoneItem(brandId);
+
+});
+
+function listPhoneItem(brandId) {
+    if (brandId === null) {
+        return;
+    }
+    $.post("/front/listPhoneItem",{brandId:parseInt(brandId)},function (data) {
+        layui.laytpl(phoneList.innerHTML).render(data,function(html){
+            $('#phone-list').html(html);
+        })
+    });
+
+}
+
+function defaultPic() {
+    return "/static/images/no_pic.jpg";
+}
